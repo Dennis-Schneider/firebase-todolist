7e51fb26d2f4ede7ba516ca4722d5ef00c2c5582
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-04-28 21:52:55 +0200

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ds-firebase-todolist',
+    appId: '1:73057175985:web:7225af046a861d16d6d590',
+    storageBucket: 'ds-firebase-todolist.appspot.com',
+    apiKey: 'AIzaSyBkgUCJAVk-u0bZmhXrJ_ea_5Z6yWKAs8w',
+    authDomain: 'ds-firebase-todolist.firebaseapp.com',
+    messagingSenderId: '73057175985',
+  },};
