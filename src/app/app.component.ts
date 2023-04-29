f22deee1433263e89c48f82b8db9dffbe865a201
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-04-29 07:34:27 +0200

    06 | Code erklärt + Todos abrufen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = '14-firebase-todolist';
+  todos$: Observable<any>;
+  todos: Array<any>;
+  constructor(firestore: Firestore) {
+    const itemCollection = collection(firestore, 'todos');
+    this.todos$ = collectionData(itemCollection);
+
+    this.todos$.subscribe((newTodos) => {
+      console.log('Neue Todos sind: ', newTodos);
+      this.todos = newTodos;
+    });
+  }
 }
